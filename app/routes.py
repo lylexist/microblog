c32ea5bd10b54e42dfac2c3adaf9434bcaf9d3f2
lyl <lylexist@gmail.com>
2018-09-05 18:36:19 +0800

    增加Profile&edit profile

--- app/routes.py
+++ app/routes.py
@@ -7,6 +7,12 @@ from flask_login import logout_user, login_required
 from werkzeug.urls import url_parse
 from datetime import datetime
 
+@app.before_request
+def before_request():
+    if current_user.is_authenticated:
+        current_user.last_seen = datetime.utcnow()
+        db.session.commit()
+
 
 @app.route('/')
 @app.route('/index')
@@ -79,14 +85,8 @@ def user(username):
     return render_template('user.html',user = user, posts=posts)
 
 
-@app.before_request
 
-def before_request():
-    if current_user.is_authenticated:
-        current_user.last_seen = datetime.utcnow()
-        db.session.commit()
-
-@app.route('/edit_profile', method=['GET','POST'])
+@app.route('/edit_profile', methods=['GET','POST'])
 @login_required
 
 def edit_profile():
